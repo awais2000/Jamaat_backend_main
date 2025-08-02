737a8e30447459a3c721bfd588ab364aee67eff6
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-08-02 00:17:30 -0700

    Update db.ts

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -6,26 +6,10 @@ dotenv.config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || "147.79.64.150",
-  user: process.env.DB_USER || "u334339390_UseRMemEentS",
-  password: process.env.DB_PASSWORD || "jAm@aTm3mB3Rs!3",
-  database: process.env.DB_NAME || "u334339390_MemberEvents",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
-// import mysql from "mysql2/promise";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "ems",
-//   port: 3306,
-//   connectTimeout: 30000,
-// });
-
-// export default pool;
